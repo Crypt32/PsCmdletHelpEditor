59cbb39e94958d2139d1b9695ca5af9ab43530ee
crypt32 <vpodans@sysadmins.lv>
2018-10-07 17:12:37 +0300

    Version bump

--- CmdletHelpEditor/Properties/AssemblyInfo.cs
+++ CmdletHelpEditor/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sysadmins LV")]
 [assembly: AssemblyProduct("CmdletHelpEditor")]
-[assembly: AssemblyCopyright("Copyright ©  2012-2015")]
+[assembly: AssemblyCopyright("Copyright ©  2012-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.4.0.0")]
-[assembly: AssemblyFileVersion("3.4.0.0")]
+[assembly: AssemblyVersion("3.4.1.0")]
+[assembly: AssemblyFileVersion("3.4.2.0")]
 [assembly: GuidAttribute("ad2db9e9-9adb-4cf8-8eee-4e252acfce5d")]
