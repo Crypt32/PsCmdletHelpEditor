68dcb02aae9fe76c31045443845aa952bd3a82e4
Vadims Podans <vpodans@sysadmins.lv>
2020-07-23 15:11:57 +0300

    added Unity

--- CmdletHelpEditor/App.xaml.cs
+++ CmdletHelpEditor/App.xaml.cs
@@ -2,18 +2,31 @@
 using System.Windows;
 using CmdletHelpEditor.API.ViewModels;
 using CmdletHelpEditor.Views.Windows;
+using Unity;
 
 namespace CmdletHelpEditor {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App {
+
+        public App() {
+            Container = new UnityContainer();
+            configureServices();
+        }
+
+        public static IUnityContainer Container { get; private set; }
+
         void ApplicationStartup(Object sender, StartupEventArgs e) {
             ((App)sender).MainWindow = new MainWindow();
             if (e.Args.Length == 1) {
                 ((MainWindowVM)((App)sender).MainWindow.DataContext).CommandManager.OpenProject(e.Args[0]);
             }
             ((App)sender).MainWindow.Show();
+        }
+
+        void configureServices() {
+            
         }
     }
 }
