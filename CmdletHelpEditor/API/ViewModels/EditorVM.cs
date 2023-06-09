b44e5923a7076eb6215045e2fcd4d40b866bcb35
Vadims Podāns <vpodans@sysadmins.lv>
2023-06-09 15:17:24 +0300

    refactoring

--- CmdletHelpEditor/API/ViewModels/EditorVM.cs
+++ CmdletHelpEditor/API/ViewModels/EditorVM.cs
@@ -1,45 +1,38 @@
 ﻿using System;
-using System.ComponentModel;
 using CmdletHelpEditor.API.Models;
+using SysadminsLV.WPF.OfficeTheme.Toolkit.ViewModels;
 
-namespace CmdletHelpEditor.API.ViewModels {
-    public class EditorVM : INotifyPropertyChanged {
-        Int32 paramIndex = -1;
-        CmdletObject currentCmdlet;
+namespace CmdletHelpEditor.API.ViewModels;
+public class EditorVM : ViewModelBase {
+    Int32 paramIndex = -1;
+    CmdletObject currentCmdlet;
 
-        public EditorVM(ClosableModuleItem parent) {
-            ParamContext = new ParamVM();
-            RelatedLinkContext = new RelatedLinkVM();
-            ExampleContext = new ExampleVM();
-            OutputContext = new OutputVM(parent);
-        }
+    public EditorVM(ClosableModuleItem parent) {
+        ParamContext = new ParamVM();
+        RelatedLinkContext = new RelatedLinkVM();
+        ExampleContext = new ExampleVM();
+        OutputContext = new OutputVM(parent);
+    }
 
-        public ParamVM ParamContext { get; set; }
-        public RelatedLinkVM RelatedLinkContext { get; set; }
-        public ExampleVM ExampleContext { get; set; }
-        public OutputVM OutputContext { get; private set; }
-        public CmdletObject CurrentCmdlet {
-            get => currentCmdlet;
-            set {
-                currentCmdlet = value;
-                ParamIndex = -1;
-                OnPropertyChanged(nameof(CurrentCmdlet));
-                ExampleContext.SetCmdlet(currentCmdlet);
-                RelatedLinkContext.SetCmdlet(currentCmdlet);
-            }
+    public ParamVM ParamContext { get; set; }
+    public RelatedLinkVM RelatedLinkContext { get; set; }
+    public ExampleVM ExampleContext { get; set; }
+    public OutputVM OutputContext { get; private set; }
+    public CmdletObject CurrentCmdlet {
+        get => currentCmdlet;
+        set {
+            currentCmdlet = value;
+            ParamIndex = -1;
+            OnPropertyChanged(nameof(CurrentCmdlet));
+            ExampleContext.SetCmdlet(currentCmdlet);
+            RelatedLinkContext.SetCmdlet(currentCmdlet);
         }
-        public Int32 ParamIndex {
-            get => paramIndex;
-            set {
-                paramIndex = value;
-                OnPropertyChanged(nameof(ParamIndex));
-            }
+    }
+    public Int32 ParamIndex {
+        get => paramIndex;
+        set {
+            paramIndex = value;
+            OnPropertyChanged(nameof(ParamIndex));
         }
-        
-        void OnPropertyChanged(String name) {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            handler?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
