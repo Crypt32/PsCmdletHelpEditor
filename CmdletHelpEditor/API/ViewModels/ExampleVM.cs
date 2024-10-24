d64f1d4070519f8ce1305e390f2956e62ee8a71c
Vadims Podāns <vpodans@sysadmins.lv>
2024-10-24 10:40:55 +0300

    minor refactoring

--- CmdletHelpEditor/API/ViewModels/ExampleVM.cs
+++ CmdletHelpEditor/API/ViewModels/ExampleVM.cs
@@ -1,91 +1,90 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Windows.Input;
 using CmdletHelpEditor.API.Models;
 using SysadminsLV.WPF.OfficeTheme.Toolkit.Commands;
+using SysadminsLV.WPF.OfficeTheme.Toolkit.ViewModels;
 
-namespace CmdletHelpEditor.API.ViewModels {
-    public class ExampleVM : INotifyPropertyChanged {
-        CmdletObject cmdlet;
-        Example currentExample;
+namespace CmdletHelpEditor.API.ViewModels;
+public class ExampleVM : ViewModelBase {
+    Boolean exampleTextBoxEnabled;
+    CmdletObject cmdlet;
+    Example currentExample;
 
-        public ExampleVM() {
-            NewExampleCommand = new RelayCommand(newExample, canNewExample);
-            RemoveExampleCommand = new RelayCommand(removeExample, canRemoveExample);
-            UpExampleCommand = new RelayCommand(upExample, canUpExample);
-            DownExampleCommand = new RelayCommand(downExample, canDownExample);
-        }
-        
-        public ICommand NewExampleCommand { get; set; }
-        public ICommand RemoveExampleCommand { get; set; }
-        public ICommand UpExampleCommand { get; set; }
-        public ICommand DownExampleCommand { get; set; }
-        public Boolean ExampleTextBoxEnabled { get; set; }
-        public Example CurrentExample {
-            get => currentExample;
-            set {
-                currentExample = value;
-                ExampleTextBoxEnabled = currentExample != null;
-                OnPropertyChanged(nameof(CurrentExample));
-                OnPropertyChanged(nameof(ExampleTextBoxEnabled));
-            }
-        }
-        
-        void newExample(Object obj) {
-            var example = new Example {
-                Name = $"Example {cmdlet.Examples.Count + 1}"
-            };
-            cmdlet.Examples.Add(example);
-            CurrentExample = example;
-        }
-        Boolean canNewExample(Object obj) {
-            return cmdlet != null;
-        }
-        void removeExample(Object obj) {
-            Int32 index = cmdlet.Examples.IndexOf(CurrentExample);
-            cmdlet.Examples.Remove(CurrentExample);
-            if (index > 0) {
-                CurrentExample = cmdlet.Examples[index - 1];
-            }
-        }
-        Boolean canRemoveExample(Object obj) {
-            return CurrentExample != null;
-        }
-        void upExample(Object obj) {
-            Int32 old = cmdlet.Examples.IndexOf(CurrentExample);
-            Example temp = cmdlet.Examples[old - 1];
-            cmdlet.Examples[old - 1] = CurrentExample;
-            cmdlet.Examples[old] = temp;
-            CurrentExample = cmdlet.Examples[old - 1];
-        }
-        Boolean canUpExample(Object obj) {
-            return canRemoveExample(null) && cmdlet.Examples.IndexOf(CurrentExample) > 0;
-        }
-        void downExample(Object obj) {
-            Int32 old = cmdlet.Examples.IndexOf(CurrentExample);
-            Example temp = cmdlet.Examples[old + 1];
-            cmdlet.Examples[old + 1] = CurrentExample;
-            cmdlet.Examples[old] = temp;
-            CurrentExample = cmdlet.Examples[old + 1];
-        }
-        Boolean canDownExample(Object obj) {
-            if (!canNewExample(null)) {
-                return false;
-            }
+    public ExampleVM() {
+        NewExampleCommand = new RelayCommand(newExample, canNewExample);
+        RemoveExampleCommand = new RelayCommand(removeExample, canRemoveExample);
+        UpExampleCommand = new RelayCommand(upExample, canUpExample);
+        DownExampleCommand = new RelayCommand(downExample, canDownExample);
+    }
 
-            Int32 count = cmdlet.Examples.Count - 1;
-            return canRemoveExample(null) && cmdlet.Examples.IndexOf(CurrentExample) < count;
+    public ICommand NewExampleCommand { get; }
+    public ICommand RemoveExampleCommand { get; }
+    public ICommand UpExampleCommand { get; }
+    public ICommand DownExampleCommand { get; }
+    public Boolean ExampleTextBoxEnabled {
+        get => exampleTextBoxEnabled;
+        set {
+            exampleTextBoxEnabled = value;
+            OnPropertyChanged();
         }
+    }
+    public Example CurrentExample {
+        get => currentExample;
+        set {
+            currentExample = value;
+            ExampleTextBoxEnabled = currentExample != null;
+            OnPropertyChanged();
+        }
+    }
 
-        public void SetCmdlet(CmdletObject newCmdlet) {
-            cmdlet = newCmdlet;
-            CurrentExample = null;
+    void newExample(Object obj) {
+        var example = new Example {
+            Name = $"Example {cmdlet.Examples.Count + 1}"
+        };
+        cmdlet.Examples.Add(example);
+        CurrentExample = example;
+    }
+    Boolean canNewExample(Object obj) {
+        return cmdlet != null;
+    }
+    void removeExample(Object obj) {
+        Int32 index = cmdlet.Examples.IndexOf(CurrentExample);
+        cmdlet.Examples.Remove(CurrentExample);
+        if (index > 0) {
+            CurrentExample = cmdlet.Examples[index - 1];
+        }
+    }
+    Boolean canRemoveExample(Object obj) {
+        return CurrentExample != null;
+    }
+    void upExample(Object obj) {
+        Int32 old = cmdlet.Examples.IndexOf(CurrentExample);
+        Example temp = cmdlet.Examples[old - 1];
+        cmdlet.Examples[old - 1] = CurrentExample;
+        cmdlet.Examples[old] = temp;
+        CurrentExample = cmdlet.Examples[old - 1];
+    }
+    Boolean canUpExample(Object obj) {
+        return canRemoveExample(null) && cmdlet.Examples.IndexOf(CurrentExample) > 0;
+    }
+    void downExample(Object obj) {
+        Int32 old = cmdlet.Examples.IndexOf(CurrentExample);
+        Example temp = cmdlet.Examples[old + 1];
+        cmdlet.Examples[old + 1] = CurrentExample;
+        cmdlet.Examples[old] = temp;
+        CurrentExample = cmdlet.Examples[old + 1];
+    }
+    Boolean canDownExample(Object obj) {
+        if (!canNewExample(null)) {
+            return false;
         }
 
-        void OnPropertyChanged(String name) {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            handler?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-        public event PropertyChangedEventHandler PropertyChanged;
+        Int32 count = cmdlet.Examples.Count - 1;
+        return canRemoveExample(null) && cmdlet.Examples.IndexOf(CurrentExample) < count;
+    }
+
+    public void SetCmdlet(CmdletObject newCmdlet) {
+        cmdlet = newCmdlet;
+        CurrentExample = null;
     }
 }
--- CmdletHelpEditor/API/ViewModels/RelatedLinkVM.cs
+++ CmdletHelpEditor/API/ViewModels/RelatedLinkVM.cs
@@ -1,88 +1,86 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Windows.Input;
 using CmdletHelpEditor.API.Models;
 using SysadminsLV.WPF.OfficeTheme.Toolkit.Commands;
+using SysadminsLV.WPF.OfficeTheme.Toolkit.ViewModels;
 
-namespace CmdletHelpEditor.API.ViewModels {
-    public class RelatedLinkVM : INotifyPropertyChanged {
-        CmdletObject cmdlet;
-        RelatedLink currentRelink;
+namespace CmdletHelpEditor.API.ViewModels;
+public class RelatedLinkVM : ViewModelBase {
+    Boolean linkTextBoxEnabled;
+    CmdletObject cmdlet;
+    RelatedLink currentRelink;
 
 
-        public RelatedLinkVM() {
-            NewLinkCommand = new RelayCommand(newLink, canNewLink);
-            RemoveLinkCommand = new RelayCommand(removeLink, canRemoveLink);
-            UpLinkCommand = new RelayCommand(upLink, canUpLink);
-            DownLinkCommad = new RelayCommand(downLink, canDownLink);
-        }
-        
-        public ICommand NewLinkCommand { get; set; }
-        public ICommand RemoveLinkCommand { get; set; }
-        public ICommand UpLinkCommand { get; set; }
-        public ICommand DownLinkCommad { get; set; }
-        public Boolean LinkTextBoxEnabled { get; set; }
-        public RelatedLink CurrentRelink {
-            get => currentRelink;
-            set {
-                currentRelink = value;
-                LinkTextBoxEnabled = currentRelink != null;
-                OnPropertyChanged(nameof(CurrentRelink));
-                OnPropertyChanged(nameof(LinkTextBoxEnabled));
-            }
-        }
+    public RelatedLinkVM() {
+        NewLinkCommand = new RelayCommand(newLink, canNewLink);
+        RemoveLinkCommand = new RelayCommand(removeLink, canRemoveLink);
+        UpLinkCommand = new RelayCommand(upLink, canUpLink);
+        DownLinkCommand = new RelayCommand(downLink, canDownLink);
+    }
 
-        void newLink(Object obj) {
-            var link = new RelatedLink {LinkText = "<Link>"};
-            cmdlet.RelatedLinks.Add(link);
-            CurrentRelink = link;
+    public ICommand NewLinkCommand { get; }
+    public ICommand RemoveLinkCommand { get; }
+    public ICommand UpLinkCommand { get; }
+    public ICommand DownLinkCommand { get; }
+    public Boolean LinkTextBoxEnabled {
+        get => linkTextBoxEnabled;
+        set {
+            linkTextBoxEnabled = value;
+            OnPropertyChanged();
         }
-        Boolean canNewLink(Object obj) {
-            return cmdlet != null;
+    }
+    public RelatedLink CurrentRelink {
+        get => currentRelink;
+        set {
+            currentRelink = value;
+            LinkTextBoxEnabled = currentRelink != null;
+            OnPropertyChanged();
         }
-        void removeLink(Object obj) {
-            Int32 index = cmdlet.RelatedLinks.IndexOf(CurrentRelink);
-            cmdlet.RelatedLinks.Remove(CurrentRelink);
-            if (index > 0) {
-                CurrentRelink = cmdlet.RelatedLinks[index - 1];
-            }
-        }
-        Boolean canRemoveLink(Object obj) {
-            return CurrentRelink != null;
-        }
-        void upLink(Object obj) {
-            Int32 old = cmdlet.RelatedLinks.IndexOf(CurrentRelink);
-            RelatedLink temp = cmdlet.RelatedLinks[old - 1];
-            cmdlet.RelatedLinks[old - 1] = CurrentRelink;
-            cmdlet.RelatedLinks[old] = temp;
-            CurrentRelink = cmdlet.RelatedLinks[old - 1];
-        }
-        Boolean canUpLink(Object obj) {
-            return canRemoveLink(null) && cmdlet.RelatedLinks.IndexOf(CurrentRelink) > 0;
-        }
-        void downLink(Object obj) {
-            Int32 old = cmdlet.RelatedLinks.IndexOf(CurrentRelink);
-            RelatedLink temp = cmdlet.RelatedLinks[old + 1];
-            cmdlet.RelatedLinks[old + 1] = CurrentRelink;
-            cmdlet.RelatedLinks[old] = temp;
-            CurrentRelink = cmdlet.RelatedLinks[old + 1];
-        }
-        Boolean canDownLink(Object obj) {
-            if (!canNewLink(null)) { return false; }
-            Int32 count = cmdlet.RelatedLinks.Count - 1;
-            return canRemoveLink(null) && cmdlet.RelatedLinks.IndexOf(CurrentRelink) < count;
-        }
+    }
 
-        public void SetCmdlet(CmdletObject newcmdlet) {
-            cmdlet = newcmdlet;
-            CurrentRelink = null;
+    void newLink(Object obj) {
+        var link = new RelatedLink { LinkText = "<Link>" };
+        cmdlet.RelatedLinks.Add(link);
+        CurrentRelink = link;
+    }
+    Boolean canNewLink(Object obj) {
+        return cmdlet != null;
+    }
+    void removeLink(Object obj) {
+        Int32 index = cmdlet.RelatedLinks.IndexOf(CurrentRelink);
+        cmdlet.RelatedLinks.Remove(CurrentRelink);
+        if (index > 0) {
+            CurrentRelink = cmdlet.RelatedLinks[index - 1];
         }
+    }
+    Boolean canRemoveLink(Object obj) {
+        return CurrentRelink != null;
+    }
+    void upLink(Object obj) {
+        Int32 old = cmdlet.RelatedLinks.IndexOf(CurrentRelink);
+        RelatedLink temp = cmdlet.RelatedLinks[old - 1];
+        cmdlet.RelatedLinks[old - 1] = CurrentRelink;
+        cmdlet.RelatedLinks[old] = temp;
+        CurrentRelink = cmdlet.RelatedLinks[old - 1];
+    }
+    Boolean canUpLink(Object obj) {
+        return canRemoveLink(null) && cmdlet.RelatedLinks.IndexOf(CurrentRelink) > 0;
+    }
+    void downLink(Object obj) {
+        Int32 old = cmdlet.RelatedLinks.IndexOf(CurrentRelink);
+        RelatedLink temp = cmdlet.RelatedLinks[old + 1];
+        cmdlet.RelatedLinks[old + 1] = CurrentRelink;
+        cmdlet.RelatedLinks[old] = temp;
+        CurrentRelink = cmdlet.RelatedLinks[old + 1];
+    }
+    Boolean canDownLink(Object obj) {
+        if (!canNewLink(null)) { return false; }
+        Int32 count = cmdlet.RelatedLinks.Count - 1;
+        return canRemoveLink(null) && cmdlet.RelatedLinks.IndexOf(CurrentRelink) < count;
+    }
 
-        void OnPropertyChanged(String name) {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            handler?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
+    public void SetCmdlet(CmdletObject newCmdlet) {
+        cmdlet = newCmdlet;
+        CurrentRelink = null;
     }
 }
