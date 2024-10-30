91c364e7c0daa5d50384909f1881f70773114b2d
Vadims Podāns <vpodans@sysadmins.lv>
2024-10-30 11:34:52 +0200

    implemented HTML and Markdown output formatters and

---
+++ src/PsCmdletHelpEditor.Core/Services/Formatters/OutputFormatterFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace PsCmdletHelpEditor.Core.Services.Formatters;
+
+/// <summary>
+/// Represents factory class for output formatters.
+/// </summary>
+public static class OutputFormatterFactory {
+    /// <summary>
+    /// Gets a HTML help formatter.
+    /// </summary>
+    /// <returns>HTML help formatter.</returns>
+    public static IHelpOutputFormatter GetHtmlFormatter() {
+        return new HtmlProcessor();
+    }
+    /// <summary>
+    /// Gets a Markdown help formatter.
+    /// </summary>
+    /// <returns>Markdown help formatter.</returns>
+    public static IHelpOutputFormatter GetMarkdownFormatter() {
+        return new MarkdownProcessor();
+    }
+}
