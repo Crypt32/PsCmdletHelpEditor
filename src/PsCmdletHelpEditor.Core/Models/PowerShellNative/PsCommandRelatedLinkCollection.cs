98599938278cc82cf3e2a476add38e7532d69720
Vadims Podāns <vpodans@sysadmins.lv>
2024-11-19 18:34:01 +0200

    handled case when there is no related links in CBH

--- src/PsCmdletHelpEditor.Core/Models/PowerShellNative/PsCommandRelatedLinkCollection.cs
+++ src/PsCmdletHelpEditor.Core/Models/PowerShellNative/PsCommandRelatedLinkCollection.cs
@@ -7,12 +7,12 @@ namespace PsCmdletHelpEditor.Core.Models.PowerShellNative;
 class PsCommandRelatedLinkCollection : ReadOnlyCollectionBase<PsCommandRelatedLink> {
     public void ImportCommentBasedHelp(PSObject cbh) {
         InternalList.Clear();
-        var cbhRelatedLink = (PSObject)cbh.Members["relatedLinks"]?.Value;
-        if (cbhRelatedLink is not null) {
-            if (cbhRelatedLink.Members["navigationLink"].Value is PSObject singlePsObject) {
+        var cbhRelatedLinks = (PSObject)cbh.Members["relatedLinks"]?.Value;
+        if (cbhRelatedLinks?.Members["navigationLink"] != null) {
+            if (cbhRelatedLinks.Members["navigationLink"]?.Value is PSObject singlePsObject) {
                 InternalList.Add(PsCommandRelatedLink.FromCommentBasedHelp(singlePsObject));
             } else {
-                InternalList.AddRange(((PSObject[])cbhRelatedLink.Members["navigationLink"].Value)
+                InternalList.AddRange(((PSObject[])cbhRelatedLinks.Members["navigationLink"].Value)
                     .Select(PsCommandRelatedLink.FromCommentBasedHelp));
             }
         }
