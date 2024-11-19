ef51a1ac49dd33dce572aac6d4fe7441fe47ba43
Vadims Podāns <vpodans@sysadmins.lv>
2024-11-19 18:53:25 +0200

    removed unused dependency property

--- CmdletHelpEditor/API/ViewModels/OutputVM.cs
+++ CmdletHelpEditor/API/ViewModels/OutputVM.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Media;
 using System.Xml.Linq;
@@ -33,12 +32,6 @@ public class OutputVM : AsyncViewModel {
     public IAsyncCommand GenerateOutputCommand { get; }
 
     public ModuleObject Tab { get; }
-
-    public static readonly DependencyProperty HtmlTextProperty = DependencyProperty.Register(
-        nameof(HtmlText),
-        typeof(String),
-        typeof(OutputVM),
-        new PropertyMetadata("<br/>"));
 
     public String HtmlText {
         get => htmlText;
